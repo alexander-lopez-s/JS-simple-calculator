a0d24c4fda030e51369bf4a981bf8ccf663830bc
Cristobal Alexander Lopez Segovia <alxlpz@outlook.com>
2023-06-09 14:59:56 +0200

    added calculator to the repository

--- src/index.js
+++ src/index.js
@@ -1,2 +1,59 @@
-// Say hello let'us build a calculator
-alert("Hello there, let'us build a calculator");
+const container = document.getElementById('calculator-container');
+const characters = [1, 2, 3, '+', 4, 5, 6, '-', 7, 8, 9, '*', 0, '.', '/'];
+const screen = document.getElementById('screen');
+const equals = document.getElementById('equals');
+
+screen.addEventListener('keyup', () => {
+    if (!/^[0-9]*$/.test(screen.value)) {
+        screen.value = 0;
+    }
+});
+
+const clear = () => {
+    screen.value = '';
+};
+
+const display = (char) => {
+    screen.value += char;
+};
+
+const result = () => {
+    const isMath = screen.value;
+    let evaluate = eval(isMath);
+    if(evaluate === undefined){
+        evaluate = '';
+    }
+    screen.value = evaluate;
+    
+};
+
+const calculatorCharacters = () => {
+    for (const char of characters) {
+        const button = document.createElement('input');
+        button.value = char;
+        button.type = 'button';
+        button.id = 'buttons';
+        button.addEventListener('click', () => {
+            display(char);
+        });
+        container.appendChild(button);
+    }
+    const clearButton = document.getElementById('clear');
+    clearButton.addEventListener('click', clear);
+};
+calculatorCharacters();
+
+equals.addEventListener('click', result);
+
+const deleteIt = () => {
+    const deleteButton = document.createElement('input');
+    deleteButton.type = 'button';
+    deleteButton.value = '↩';
+    deleteButton.id = 'delete';
+    deleteButton.addEventListener('click', () => {
+        screen.value = screen.value.slice(0, -1);
+    });
+    container.appendChild(deleteButton);
+};
+deleteIt();
+
